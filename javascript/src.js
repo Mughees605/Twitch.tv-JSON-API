6312ec867bffcad45ad95da4d0a72bfa69b77067
mughees605 <mughees605@gmail.com>
2016-12-28 20:25:43 +0200

    complete

--- javascript/src.js
+++ javascript/src.js
@@ -1,78 +1,44 @@
-// // $(document).ready(function(){
+ $(document).ready(function(){
+
   var arr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-// //   for(var i = 0 ; i < arr.length; i++){
-
-// // var url = "https://api.twitch.tv/kraken/streams/"+arr[i]+"";
-  
-// // $.ajax(url,{
-// // type:"GET",
-// //   headers:{
-// //   "Client-ID":"3xuge9gq2tx8oeec59bh4dudj3ozau"
-// // },
-// // success:function(data){
-        
-// //       console.log(data);
-
-
-// //     // for(var i = 0; i <= data.follows.length; i++){
-// //     //   var status = data.follows[i].channel.status
-// //     //   var name = data.follows[i].channel.name.toUpperCase();
-// //     //   var logo = data.follows[i].channel.logo;
-// //     //   var link = data.follows[i].channel.url;
-// //     //   var noImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2000px-No_image_available.svg.png";
-      
-      
-// //     //    if(status != null){
-// //     //      $("ul").append("<li class='w3-padding-55'> <img src="+logo+" class='w3-left w3-circle w3-margin-right' /> <a href="+link+"><span class='w3-xlarge'>" +name+ " is online</span></a> </li>")
-// //     //    }
-// //     //     else{
-// //     //       $("ul").append("<li class='w3-padding-55'><img src="+noImg+" class='w3-left w3-circle w3-margin-right' />  <a href="+link+"><span class='w3-xlarge'> " +name+ " is offline</span></a></li> ")
-// //     //     }
-// //     // }
-// // }
-// // });
-// //   }  
-
-// // })
-// console.log(32);
-// for(var i = 0 ; i < arr.length; i++){
-// var url = [];
-//  url.push('https://api.twitch.tv/kraken/streams/' + arr[i] + '?client_id=3xuge9gq2tx8oeec59bh4dudj3ozau&callback=?');
-// }
-// for(var j = 0 ; j < url.length; j++){
-// $.getJSON(url[j],function(data){
-//   console.log(url[j]);
-  
-  
-// })
-// }
- streamurl = "https://api.twitch.tv/kraken/streams/",
- userurl = "https://api.twitch.tv/kraken/users/",
- arr1 = [];
+  streamurl = "https://api.twitch.tv/kraken/streams/",
+  userurl = "https://api.twitch.tv/kraken/users/",
+ 
 arr.forEach(function(user){
   $.getJSON(userurl + user + "?client_id=3xuge9gq2tx8oeec59bh4dudj3ozau&callback=?",function(result){
   $.getJSON(streamurl + user + "?client_id=3xuge9gq2tx8oeec59bh4dudj3ozau&callback=?",function(data){
 
     if(data.stream == null){
-       arr1.push(user);
-      $("ul").append("<a href='https://www.twitch.tv/"+user+"'><li> " +user+ " is offline </li></a>")
+  
+      $("ul").append("<li class='not_active' class = 'w3-padding-32'><a href='https://www.twitch.tv/"+user+"' > " +user+ " is offline </a></li>")
     }
     else{
-       var channelURL = this.stream.channel.url;
-
-  $("ul").append("<a href="+channelURL+"><li>" +user+ " is online</li></a>")
+       var channelURL = data.stream.channel.url;
+  $("ul").append("<li class='active'><a href="+channelURL+" >" +user+ " is online</a></li>");
     }
+
+///////////////////////////////////// online function start
+    $(".online").on('click',function(){
+      $("li.not_active").hide();
+      $("li.active").show();
+    })
+///////////////////////////////////////// online function end
+
+////////////////////////////////////////  offline function start
+    $(".offline").on("click",function(){
+        $("li.active").hide();
+        $("li.not_active").show();
+    });
+///////////////////////////////////////  offline function ends
+     
+//all functon start 
+     $(".all").on('click',function(){
+       $("li.not_active").show();
+      $("li.active").show();
+     })
+// all function ends
   }) // data ends
   }); // result ends
 }); // user ends
 
-// onclick function 
-function online(arr){
-  var arg = Array.prototype.slice.call(arguments);
-  var arg2 = Array.prototype.slice.call(arg);
-  console.log(arg2);
-
-}
-online(arr1);
-
-// end onclick function
+ })
